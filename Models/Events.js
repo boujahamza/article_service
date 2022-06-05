33e3f2d3ffbc89c9a1f1095c7b92914d7d4dc0a7
Boukedir Mohamed <bouukedir@outlook.com>
2022-06-05 18:24:50 +0100

    add dockerfile

--- Models/Events.js
+++ Models/Events.js
@@ -4,7 +4,10 @@ const Schema =mongoose.Schema;
 const eventSchema = new Schema({
     poster_url:String,
     event_title:String,
-    contestants:Array
+    contestants:Array,
+    contestant_id:Array,
+    date:Date,
+    tags:Array,
 },{timestamps :true});
 
 var event=mongoose.model('event',eventSchema);
